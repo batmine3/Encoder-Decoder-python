941a024ee53bde897f5f40fffb3b61aca95a8c30
Baptiste <batmine3@gmail.com>
2019-11-21 15:03:59 +0100

    Add ";"

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
     int value;
     printf("Enter a value: ");
     scanf("%d", &value);
-    printf("You have set value to %d", value)
+    printf("You have set value to %d", value);
     return 0;
 }
