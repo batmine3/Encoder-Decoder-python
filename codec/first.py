dcb96c85bc14613fc34f63520954bd78b85b0f26
Batmine3 <batmine3@gmail.com>
2019-12-14 11:30:45 +0100

    open file

--- codec/first.py
+++ codec/first.py
@@ -5,5 +5,10 @@ import getpass
 matrice = input("Entrez le nom de la matric à selectionner : ")
 print(matrice)
 user = getpass.getuser()
-
 print(user)
+fileOpend = open("/home/"+user+"/Projet-Secu-Perso/codec/key", "r")
+if fileOpend ==NULL{
+    print("KO")
+}else{
+    print("OK")
+}
